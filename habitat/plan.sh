e617c34afdc7dd5fe336b9d579004cb7a8164fc2
equalitism <aden1020@hotmail.com>
2020-09-23 06:53:03 -0400

    v0.2.0, obtain imagemagick pkg_deps from my origin

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,4 +2,4 @@ pkg_name=meme-machine
 pkg_origin=aden_tryhabitat
 pkg_version="0.2.0"
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_deps=( MYORIGIN/imagemagick )
+pkg_deps=( aden_tryhabitat/imagemagick )
